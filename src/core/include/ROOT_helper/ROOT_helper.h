ea1fc2a7eb24709a4d1eb6793bc9039ad71c7d5a
SHU Kenji <kenji0923@gmail.com>
2025-02-02 16:57:15 +0900

    Add necessary header for compiled project

---
+++ src/core/include/ROOT_helper/ROOT_helper.h
@@ -0,0 +1,11 @@
+#ifndef ROOT_HELPER_H
+#define ROOT_HELPER_H
+
+
+#include <ROOT_helper/data_saver.h>
+#include <ROOT_helper/graphics.h>
+#include <ROOT_helper/container.h>
+#include <ROOT_helper/analysis.h>
+
+
+#endif
